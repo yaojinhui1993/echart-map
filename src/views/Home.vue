d8e30963c69ef821c8cf43c41b8afde34be83cc7
yaojinhui <yaojinhui1993@gmail.com>
2018-12-07 14:31:14 +0800

    Add router

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">Select chart!</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
